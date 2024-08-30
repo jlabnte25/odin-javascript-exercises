314c392e8c40db0ec0708b296666031f92f3e9f8
July <majuliabonite@gmail.com>
2024-08-31 00:11:38 +0800

    Accomplished exercised 5 or removeFromArray

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
 
-};
+
+const sumAll = function(a=0,b=0) {
+    
+    if (a>=0 && b>=0 && (a- Math.floor(a))===0 && (b - Math.floor(b))===0 && typeof a !== 'string' && typeof b !== 'string') {
+    let smaller = (a < b) ? a : b;
+    let bigger = (a > b) ? a : b;
+
+    function sumEquation (smaller,bigger) {
+        let sum = (((bigger - smaller + 1) / 2)*(bigger+smaller));{
+        return sum;}    
+
+    }return sumEquation(smaller,bigger);}
+
+    else {
+        return "ERROR";
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
