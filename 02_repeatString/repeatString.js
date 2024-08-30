a14ddecc64bc3215b89fde0fd4a1242907306ec3
July <majuliabonite@gmail.com>
2024-08-30 18:49:26 +0800

    Accomplish repeatString and reverseString exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,27 @@
-const repeatString = function() {
 
-};
+
+
+const repeatString = function(stringInput, i = 0) {
+    let stringRepeat = [];
+    let j = 0;
+    
+    if (i==0) {
+        stringRepeat = "";
+        return stringRepeat;
+    } else if (i<0){
+        stringRepeat = "ERROR";
+        return stringRepeat;
+    } else if (j<i) {for(j = 0 ; j < i ; j++){
+        stringRepeat.push(stringInput);
+        } return stringRepeat.join('');
+    } else if (j==i) {let repeatString = ""};
+}
 
 // Do not edit below this line
 module.exports = repeatString;
+
+//if (j<i) {for(j = 0 ; j < i ; j++){
+    //stringRepeat.push(stringInput);
+    //} return stringRepeat.join('');
+//} else if (j==i) {let repeatString = ""};
+//}
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+let string = "";
 
-};
+const reverseString = function(string = "") {{
+    let stringArray = []; 
+        for (let i=0; i < string.length; i++) {
+        stringArray.push(string.charAt(i));
+        }
+    
+    function reverseArray() {
+        let newString = [];
+        for (let j=string.length-1 ; j>=0 ; j--) {
+            newString.push(stringArray[j]);
+        }
+        return newString.join("");}
+    }
+    return reverseArray();
+}
+
 
 // Do not edit below this line
 module.exports = reverseString;
