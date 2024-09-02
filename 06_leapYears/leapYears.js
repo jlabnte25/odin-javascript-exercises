6a50a822ee08ce7e8df2eff80acc8401a7142d69
July <majuliabonite@gmail.com>
2024-09-02 15:35:39 +0800

    Accomplish exercise 6 or leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year=0) {
 
-};
+    let result = 0;
 
+    if ((year % 400 == 0) && (year % 4 == 0)) {
+        result = true;
+    } else if (year % 100 == 0) {
+        result = false;
+    } else if (year % 4 == 0) {
+        result = true;
+    } else {
+        result= false;
+    }return result;
+
+}
 // Do not edit below this line
 module.exports = leapYears;
