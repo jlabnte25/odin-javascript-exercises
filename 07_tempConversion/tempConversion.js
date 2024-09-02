d3e7fa25e7741e4a9665847e01d4a126f359e575
July <majuliabonite@gmail.com>
2024-09-02 15:59:11 +0800

    Accomplish exercise 7 or tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+   let C = (5/9) * (F -32);
+   let CRounded = Math.round(C*10)/10;
+   return CRounded;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let F = (9/5 * (C)) +32;
+  let FRounded = Math.round(F*10)/10;
+  return FRounded;
 };
 
 // Do not edit below this line
